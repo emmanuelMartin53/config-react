782f890102b446fe2abe8ac07515c39e0d994aa5
Emmanuel Martin <em.martin456@gmail.com>
2025-09-22 14:24:08 +0200

    config webpack + package.json

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Salut les amis</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import des packages
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// import des fichiers
+import App from "./components/App";
+
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />)
